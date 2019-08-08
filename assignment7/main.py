626b4cfe03f29792e687daeb1f96638ea89e7cc1
Computer Education <>
2019-08-08 16:06:37 +0700

    dung

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def member():
     return render_template('meaw.html')
+
+@app.route('/',methods=['post'])
+def Register():
+    return 'Register Ok Hahahaha';
 
 if __name__ == "__main__":
     app.run(debug=True)
