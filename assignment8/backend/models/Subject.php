8783ef071630dd52d51681da9569940531df1323
KanitthaKointa <52272954+KanitthaKointa@users.noreply.github.com>
2019-10-03 15:56:27 +0700

    add

---
+++ assignment8/backend/models/Subject.php
@@ -0,0 +1,15 @@
+
+<?php
+namespace backend\models;
+    use yii\behaviors\TimestampBehavior;
+    use yii\behaviors\BlameableBehavior;
+class Subject extends \common\models\Subject
+{
+    public function behaviors()
+    {
+        return [
+            ['name', 'required', 'message' => 'please insert data'],
+            ['name', 'email', 'message' => 'please insert type example@email.com']
+        ];
+    }
+}
